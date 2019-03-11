876081dfb5eeff13bc3a081223fc344346444fc9
Daisuke Shiroma <13da144@gmail.com>
2019-03-11 21:03:06 +0900

    first

---
+++ Pods/Target Support Files/Pods-realmstudy/Pods-realmstudy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_realmstudy : NSObject
+@end
+@implementation PodsDummy_Pods_realmstudy
+@end
---
+++ Pods/Target Support Files/Pods-realmstudyTests/Pods-realmstudyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_realmstudyTests : NSObject
+@end
+@implementation PodsDummy_Pods_realmstudyTests
+@end
